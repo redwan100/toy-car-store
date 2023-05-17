c389568d3ca0bc068e5294263c697de5727819f2
Redwan islam <redwanislam2760@gmail.com>
2023-05-17 21:46:24 +0600

    added context provider

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 >Home Page</h1>
+      <div className='gradient'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia provident at iste accusantium? Placeat, eius optio ipsa fuga in dolorum corporis consectetur veritatis. Tempora libero obcaecati inventore quam, nulla modi.
+      </div>
+    </div>
+  )
+}
+
+export default Home
